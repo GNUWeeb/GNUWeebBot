a27b3a32e50bda2dfdf97ba4ff7353cae59d546a
Ammar Faizi <ammarfaizi2@gmail.com>
2021-03-22 06:59:51 +0700

    Update help message
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/gwbot/help.c
+++ src/gwbot/help.c
@@ -4,5 +4,15 @@
 
 void print_gwbot_help(const char *app)
 {
+	printf("GNUWeebBot " GWBOT_VERSION "\n\n");
 	printf("Usage: %s -c <config_file>\n", app);
+	printf("\n");
+	printf("[Basic]\n");
+	printf("  -h, --help\t\t\tShow this help message\n");
+	printf("  -v, --version\t\t\tShow app version\n");
+	printf("  -c, --config=FILE\t\tSet config file\n");
+	printf("\n");
+	printf("[Credential]\n");
+	printf("  -t, --token=TOKEN\t\tSet bot token\n");
+	printf("\n");
 }
