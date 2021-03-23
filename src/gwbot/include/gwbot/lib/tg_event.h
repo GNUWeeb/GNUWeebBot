bc1ef39f4bca10de142ad47db5738cca75277cda
Aliwoto <aminnimaj@gmail.com>
2021-03-23 13:06:28 -0400

    Lol, I just forgot to add header files :|

---
+++ src/gwbot/include/gwbot/lib/tg_event.h
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  src/gwbot/include/gwbot/lib/tg_event.h
+ *
+ *  Recieve message header for gwbot
+ *
+ *  Copyright (C) 2021  aliwoto
+ */
+
+#ifndef GWBOT__LIB__TG_EVENT_H
+#define GWBOT__LIB__TG_EVENT_H
+
+#include <stdlib.h>
+#include <gwbot/base.h>
+
+/* 
+ * The kind of the message event.
+ */
+typedef enum _msg_kind_t {
+	TEXT_MSG        = 0,
+	PHOTO_MSG       = 1,
+	STICKER_MSG     = 2
+} _msg_kind_t;
+
+
+/* 
+ * The Event.
+ */
+typedef struct _tg_event {
+        uint64_t        update_id;
+        _tg_event_msg   tg_msg;
+
+} _tg_event;
+
+
+/* 
+ * The event message.
+ */
+typedef struct _tg_event_msg {
+        bool            is_main_msg;
+        const char      *body;
+        _msg_kind_t     msg_kind;
+} _tg_event;
+
+
+/* 
+ * The event sender.
+ */
+typedef struct _tg_msg_sender {
+    int64_t msg_id;
+
+
+} _tg_msg_sender;
+
+
+/* 
+ * The chat room of the message event.
+ */
+typedef struct _tg_event_chat {
+    int64_t msg_id;
+
+
+} _tg_event_chat;
+
+
+
+
+
+
+
+
+
+
+#endif /* #ifndef GWBOT__LIB__TG_EVENT_H */
---
+++ src/gwbot/include/gwbot/lib/tg_event/text.h
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  src/gwbot/include/gwbot/lib/tg_api/send_message.h
+ *
+ *  String helpers header for TeaVPN2
+ *
+ *  Copyright (C) 2021  Ammar Faizi
+ */
+
+#ifndef GWBOT__LIB__TG_EVENT__TEXT_H
+#define GWBOT__LIB__TG_EVENT__TEXT_H
+
+#include <gwbot/lib/tg_event.h>
+
+/* 
+ * The kind of the message event.
+ */
+typedef enum _tg_text_type_t {
+	ITALIC_TEXT_MSG         = 0,
+	BOLD_TEXT_MSG           = 1,
+	COMMAND_TEXT_MSG        = 2
+} _msg_kind_t;
+
+/* 
+ * The event text message.
+ */
+typedef struct _tg_text_msg {
+        const char      *text;
+        bool            has_entities;
+        bool            is_replied;
+        uint64_t        message_id;
+        _tg_msg_sender  sender;
+        _tg_event_chat  chat_room;
+        uint64_t        date;
+        _tg_msg_entity  entity;
+} _tg_text_msg;
+
+
+/* 
+ * The entity of the text message.
+ */
+typedef struct _tg_msg_entity
+{
+        uint32_t        offset;
+        uint32_t        length;
+        _tg_text_type_t text_type;
+} _tg_msg_entity;
+
+
+
+static inline _tg_text_msg *tg_get_text_msg(_tg_event_msg *msg)
+{
+        if (msg->msg_kind != TEXT_MSG)
+        {
+                pr_err("ERR: TRYING TO GET A TEXT FROM NON-TEXT KIND EVENT!");
+                return NULL;
+        }
+        /*
+        * TODO:
+        * we should convert the _tg_event_msg to a _tg_text_msg here.
+        * 
+        */
+}
+
+
+
+#endif /* #ifndef GWBOT__LIB__TG_EVENT__TEXT_H */
