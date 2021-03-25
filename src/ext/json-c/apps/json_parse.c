25b06a165bd4153e4dfd1c58e506e747d6ee39ae
Ammar Faizi <ammarfaizi2@gmail.com>
2021-03-25 13:49:58 +0700

    Fix JSON-C build error
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/ext/json-c/apps/json_parse.c
+++ src/ext/json-c/apps/json_parse.c
@@ -155,7 +155,7 @@ static void usage(const char *argv0, int exitval, const char *errmsg)
 	exit(exitval);
 }
 
-int my_main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	json_object *new_obj;
 	int opt;
--- src/ext/json-c/json_config.h
+++ src/ext/json-c/json_config.h
@@ -1,3 +1,7 @@
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef JSON_C_HAVE_INTTYPES_H
+// #undef JSON_C_HAVE_INTTYPES_H
+
+#if __has_include("inttypes.h")
+#define JSON_C_HAVE_INTTYPES_H 1
+#endif
