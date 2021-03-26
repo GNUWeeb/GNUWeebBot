fb5fcf1e69b41655c614d0f896bd7888a1820cac
Ammar Faizi <ammarfaizi2@gmail.com>
2021-03-26 15:38:42 +0700

    Prepare gwthread wrapper
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ src/gwbot/include/gwbot/gwthread.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  src/gwbot/include/gwbot/gwthread.h
+ *
+ *  GNUWeebBot thread wrapper
+ *
+ *  Copyright (C) 2021  Ammar Faizi
+ */
+
+#ifndef GWBOT__PRINT_H
+#define GWBOT__PRINT_H
+
+
+
