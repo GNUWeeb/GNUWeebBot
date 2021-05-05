3cb357104031e4c20fa2fb6a56d0124067984f3f
Ammar Faizi <ammarfaizi2@gmail.com>
2021-05-05 21:44:22 +0700

    Prepare get chat administrators API
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/gwbot/include/gwbot/lib/tg_api/get_chat_administrators.h
+++ src/gwbot/include/gwbot/lib/tg_api/get_chat_administrators.h
@@ -13,27 +13,7 @@
 #include <gwbot/lib/tg_api.h>
 
 
-typedef enum _parse_mode_t {
-	PARSE_MODE_OFF = 0,
-	PARSE_MODE_HTML = 1,
-	PARSE_MODE_MARKDOWN = 2
-} parse_mode_t;
-
-
-struct tga_send_msg {
-	int64_t		chat_id;
-	uint64_t	reply_to_msg_id;
-	const char	*text;
-	struct_pad(0, 4);
-	parse_mode_t	parse_mode;
-};
-
-
-int ftga_get_chat_administrators(tg_api_handle *handle,
-				 struct tga_chat_member **admins);
-
-
-#define ftga_get_chat_admins ftga_get_chat_administrators
+int tga_get_chat_admins(tga_handle_t *handle, int64_t chat_id);
 
 
 #endif /* #ifndef GWBOT__LIB__TG_API__GET_CHAT_ADMINISTRATORS_H */
--- src/gwbot/lib/tg_api/get_chat_administrators.c
+++ src/gwbot/lib/tg_api/get_chat_administrators.c
@@ -1,20 +1,23 @@
 
-
 #include <gwbot/base.h>
 #include <gwbot/lib/string.h>
 #include <gwbot/lib/tg_api.h>
 #include <gwbot/lib/tg_api/get_chat_administrators.h>
 
 
-int ftga_get_chat_administrators(tg_api_handle *handle,
-				 struct tga_chat_member **admins)
+int tga_get_chat_admins(tga_handle_t *handle, int64_t chat_id)
+{
+	char buf[64];
+
+	snprintf(buf, sizeof(buf), "chat_id=%" PRId64, chat_id);
+
+	tga_set_body(handle, buf);
+	tga_set_method(handle, "getChatAdministrators");
+	return tg_api_post(handle);
+}
+
+int parse_tga_admins(const char *res)
 {
 
-
-
-
-
-
-	return 0;
 }
 
